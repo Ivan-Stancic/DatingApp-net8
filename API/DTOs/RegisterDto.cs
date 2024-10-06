1f99feec2b2632c1cd5dabd5bb6f188dd32dcb07
Ivan-Stancic <ivanstancic983@gmail.com>
2024-10-06 18:12:06 +0200

    End of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,8 +8,11 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        public required string UserName { get; set; }
-        public required string Password { get; set; }
+        [Required]
+        public string UserName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
+        public string Password { get; set; } = string.Empty;
 
     }
 }
